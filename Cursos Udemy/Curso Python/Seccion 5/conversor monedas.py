74184190f73048247aad639712ccc6047dafc92a
unknown <davidcastrillon04@gmail.com>
2023-03-24 12:40:12 -0500

    Creacion de un menu en el conversor de monedas

--- Cursos Udemy/Curso Python/Seccion 5/conversor monedas.py
+++ Cursos Udemy/Curso Python/Seccion 5/conversor monedas.py
@@ -7,7 +7,28 @@ def convertir(valor_dolar, pais):
     print(f'tienes ${dolares} Dolares')
     
 def main():
-    convertir(4742.54, 'pesos colombianos')
+    while True:
+        menu = '''
+        1. Soles Perunaos a Dolares
+        2. Pesos Mexicanos a Dolares
+        3. Pesos Colombianos a Dolares
+        4. Salir
+        INGRESE UNA OPCION: '''
+        
+        opcion = input(menu)
+        
+        if opcion == '1':
+            convertir(3.77,'soles Peruanos')
+        elif opcion == '2':
+            convertir(18,48,'pesos Mexicanos')
+        elif opcion == '3':
+            convertir(4742.54, 'pesos Colombianos')
+        elif opcion == '4':
+            print('Cerrando el conversor de monedas')
+            break
+        else:
+            print('Opcion INCORRECTA')
+            print('Ingrese una opcion entre 1 - 4')
     
 if __name__ == '__main__':
     main()
