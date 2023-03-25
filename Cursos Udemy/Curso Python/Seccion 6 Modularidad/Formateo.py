5fb9ae47cecd87ddcd14837b8a7fd526d9a95d4d
unknown <davidcastrillon04@gmail.com>
2023-03-25 13:14:45 -0500

    Diferentes tipos de salida de print

---
+++ Cursos Udemy/Curso Python/Seccion 6 Modularidad/Formateo.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+if len(argv) == 4 :
+    nombre = argv[1]
+    edad = int(argv[2])
+    altura = float(argv[3])
+    
+    #print(f'Nombre: {nombre} \nEdad: {edad} \nAltura: {altura}')
+    #print('Nombre: {} \nEdad: {} \nAltura: {}'.format(nombre, edad, altura))
+    #print('Nombre: {n} \nEdad: {e} \nAltura: {a}'.format(n = nombre, e = edad, a = altura))
+    print('Nombre: %s \nEdad: %i \nAltura: %'%(nombre, edad, altura))
+
+else:
+    print('Error, Ingrese los argumentos correctamente')
+    print('Ejemplo: entrada script.py "Texto" 5')
