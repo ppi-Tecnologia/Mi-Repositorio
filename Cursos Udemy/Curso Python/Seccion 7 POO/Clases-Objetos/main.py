b72ffe7650f311105c129466de83caed3b01a808
unknown <davidcastrillon04@gmail.com>
2023-04-07 12:57:40 -0500

    Creando constructores

--- Cursos Udemy/Curso Python/Seccion 7 POO/Clases-Objetos/main.py
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Clases-Objetos/main.py
@@ -1,13 +1,6 @@
 from persona import Persona
 
-persona1 = Persona()
-persona1.nombre = 'Juan'
-persona1.edad = 17
-
-persona2 = Persona()
-persona2.nombre = 'David'
-persona2.edad = 40
-
-persona2.mostrar_datos()
-print('='*25)
+persona1 = Persona('Jeseva', 77)
 persona1.mostrar_datos()
+
+print(persona1)
--- Cursos Udemy/Curso Python/Seccion 7 POO/Clases-Objetos/persona.py
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Clases-Objetos/persona.py
@@ -1,9 +1,17 @@
 
 class Persona:
-    nombre = None
-    edad = None
+    #nombre = None
+    #edad = None
+    
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
     
     def mostrar_datos(self):
         print('Nombre:', self.nombre)
         print('Edad:', self.edad)
+        
+    def __str__(self):
+        return f'Nombre: {self.nombre} \nEdad: {self.edad}'
         
