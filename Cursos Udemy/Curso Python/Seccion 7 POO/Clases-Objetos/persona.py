dfd114ebbdc1bdd16818bf05aa45b8bfbb869173
unknown <davidcastrillon04@gmail.com>
2023-04-08 17:35:34 -0500

    Introduccion al encapsulamiento

--- Cursos Udemy/Curso Python/Seccion 7 POO/Clases-Objetos/persona.py
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Clases-Objetos/persona.py
@@ -3,6 +3,7 @@ class Persona:
     #nombre = None
     #edad = None
     
+    #self siempre debe ir como parametro y al referirnos a una variable del metodo
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
@@ -12,6 +13,7 @@ class Persona:
         print('Nombre:', self.nombre)
         print('Edad:', self.edad)
         
+    #Este metodo __str__ lo que hace es un metodo como si fuera mostrar datos
     def __str__(self):
         return f'Nombre: {self.nombre} \nEdad: {self.edad}'
         
---
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Encapsulamiento/main.py
@@ -0,0 +1,7 @@
+from persona import Persona
+
+persona1 = Persona('Juan', 17)
+persona1.set_nombre('Castrillon')
+persona1.set_edad(18)
+print(persona1.get_nombre())
+print(persona1)
