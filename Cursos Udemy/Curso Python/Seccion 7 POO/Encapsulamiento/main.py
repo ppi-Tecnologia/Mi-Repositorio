de48d37a4642e3b2659f1c7bfc254a1cf85ae8c9
unknown <davidcastrillon04@gmail.com>
2023-04-08 18:17:00 -0500

    Poniendo comentarios

--- Cursos Udemy/Curso Python/Seccion 7 POO/Encapsulamiento/main.py
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Encapsulamiento/main.py
@@ -1,7 +1,11 @@
 from persona import Persona
 
+#creando un  objeto, nombre = Juan, edad = 17
 persona1 = Persona('Juan', 17)
+#Cambiando el nombre a Castrillon
 persona1.set_nombre('Castrillon')
+#Cambiando edad a 18
 persona1.set_edad(18)
+# imprimiendo la variable nombre del objeto persona1
 print(persona1.get_nombre())
 print(persona1)
