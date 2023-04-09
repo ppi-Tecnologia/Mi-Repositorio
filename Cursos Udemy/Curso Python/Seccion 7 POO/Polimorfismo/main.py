1475eb6b901285ab447abecf9ecb22b97ba010f0
unknown <davidcastrillon04@gmail.com>
2023-04-08 22:35:58 -0500

    Introduccion al Polimorfismo

---
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Polimorfismo/main.py
@@ -0,0 +1,10 @@
+from persona import Persona, Ciclista, Atleta
+
+persona = Persona('Juan')
+persona.moverse()
+
+atleta = Atleta('David')
+atleta.moverse()
+
+ciclista = Ciclista('Castri')
+ciclista.moverse()
