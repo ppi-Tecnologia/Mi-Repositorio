e6bc0b672fb1e230ed35e8ffd948128190e43211
unknown <davidcastrillon04@gmail.com>
2023-04-08 19:51:38 -0500

    Super

--- Cursos Udemy/Curso Python/Seccion 7 POO/Herencia/main.py
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Herencia/main.py
@@ -1,7 +1,15 @@
-from persona import Cliente
+from persona import Cliente, Empleado
 
 cliente1 = Cliente('Javier', 34)
 cliente2 = Cliente('Gilberto', 77)
 cliente1.detalle_persona()
 
 print(cliente2)
+
+empleado1 = Empleado('Alex', 25, 1160000)
+empleado2 = Empleado('Roel', 25, 1300000)
+
+empleado1.detalle_persona()
+empleado1.detalle_empleador()
+
+print(empleado2)
--- Cursos Udemy/Curso Python/Seccion 7 POO/Herencia/persona.py
+++ Cursos Udemy/Curso Python/Seccion 7 POO/Herencia/persona.py
@@ -8,7 +8,22 @@ class Persona:
         print(f'Nombre: {self.nombre} \nEdad: {self.edad}')
         
     def __str__(self):
-        return f'Nombre: {self.nombre} \nEdad: {self.edad}'
+        return f'Nombre: {self.nombre} \nEdad: {self.edad}' 
     
+#Creando una clase Cliente que era los metodos y el constructor de la clase Persona 
 class Cliente(Persona):
     pass
+
+#Clase Empleado que era de la clase Persona
+class Empleado(Persona):    
+    
+    def __init__(self, nombre, edad, sueldo):
+        super().__init__(nombre, edad)
+        self.sueldo = sueldo
+        
+    def detalle_empleador(self):
+        super().detalle_persona(    )
+        print(f'Sueldo: {self.sueldo}')
+    
+    def __str__(self):
+        return super().__str__() + f'\nSueldo: {self.sueldo}'
